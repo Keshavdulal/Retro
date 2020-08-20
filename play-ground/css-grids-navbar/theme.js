1f7c73f8ce2d51a6bacc798bc19203f3922f7bed
keshavdulal <keshav.dulal@gmail.com>
2020-08-20 15:08:23 +0545

    master : themify page

--- play-ground/css-grids-navbar/theme.js
+++ play-ground/css-grids-navbar/theme.js
@@ -7,11 +7,12 @@ const themeMap = {
 const theme =
   localStorage.getItem("theme") ||
   ((tmp = Object.keys(themeMap)[0]), localStorage.setItem("theme", tmp), tmp);
+
 const bodyClass = document.body.classList;
+
 bodyClass.add(theme);
 
 function toggleTheme() {
-  console.log("@toggleTheme");
   const current = localStorage.getItem("theme");
   const next = themeMap[current];
 
